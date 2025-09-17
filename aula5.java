21be7fbdcb8f7d0e6a8763ca4d36aa03f9cca188
nicolasglaeser <nicolas60705@edu.unipar.br>
2025-09-16 22:25:47 -0300

    Add files via upload

---
+++ aula5.java
@@ -0,0 +1,13 @@
+
+public class aula5 {
+
+public static void main(String[] args) {
+    
+    int cont;
+    cont=0;
+    while (cont < 20) {
+        System.out.println("contador: " + cont);
+        cont = cont + 1;
+    }
+}
+}
