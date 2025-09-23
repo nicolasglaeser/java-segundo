c0e40139978ec9b93f29b82d2a77f8708b4a9573
nicolasglaeser <nicolas60705@edu.unipar.br>
2025-09-23 20:34:32 -0300

    Add files via upload

---
+++ codigos-prog/Metodologias Ágeis/trabalho.js
@@ -0,0 +1,6 @@
+function showSection(sectionId) {
+    document.querySelectorAll('section').forEach(section => {
+        section.classList.remove('active');
+    });
+    document.getElementById(sectionId).classList.add('active');
+}
